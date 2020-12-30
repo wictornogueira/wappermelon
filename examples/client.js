7ef656542d5d2408b81d2fcfda6c20f558c8b815
Wictor Nogueira <wictorgoesnogueira@gmail.com>
2020-12-30 19:13:32 -0300

    updated example

--- examples/client.js
+++ examples/client.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const qrcode = require('qrcode-terminal')
 const { WappermelonClient, CommandHandler } = require('wappermelon')
 
 class ExampleClient extends WappermelonClient {
@@ -13,6 +14,14 @@ class ExampleClient extends WappermelonClient {
 client = new ExampleClient({ session: 'session.json' })
 client.initialize()
 
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true }, qr => {
+    console.log(`Scan the following QR Code as you would to login to WhatsApp Web:\n${qr}`)
+  })
+})
+
+client.on('auth-failure', console.log)
+
 client.on('ready', () => {
-  console.log('I\' ready!')
+  console.log(`Connected as ${client.info.pushname} (${client.info.me.user}).\nDone in ${process.uptime().toFixed(1)} seconds.`)
 })
